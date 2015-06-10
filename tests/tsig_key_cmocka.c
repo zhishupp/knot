1907d01379d1ab18620b067a904a15b7cbdfa150
Vítězslav Kříž <vitezslav.kriz@nic.cz>
2015-06-10 16:00:50 +0200

    Rewrite test tsig_key to use library cmocka

---
+++ tests/tsig_key_cmocka.c
@@ -0,0 +1,331 @@
+/*  Copyright (C) 2015 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <unistd.h>
+
+#include "libknot/errcode.h"
+#include "libknot/tsig.h"
+
+
+int setup(void **state)
+{
+	knot_tsig_key_t *key = malloc(sizeof(knot_tsig_key_t));
+	if (key == NULL){
+		return 1;
+	}
+	memset(key, 0, sizeof(knot_tsig_key_t));
+	*state = key;
+	return 0;
+}
+
+int teardown(void **state)
+{
+	knot_tsig_key_deinit((knot_tsig_key_t *)*state);
+	free(*state);
+	return 0;
+}
+
+
+void key_init_missing_name(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init(&key,"hmac-md5", NULL, "Wg==");
+	assert_int_equal(r, KNOT_EINVAL);
+	r = knot_tsig_key_init(&key,"hmac-md5", "", "Wg==");
+	assert_int_not_equal(r, KNOT_EOK);
+}
+
+void key_init_missing_secret(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init(&key,"hmac-md5", "name", NULL);
+	assert_int_equal(r, KNOT_EINVAL);
+}
+
+void key_init_ivalid_hmac(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init(&key,"hmac-md5", "name", NULL);
+	assert_int_equal(r, KNOT_EINVAL);
+}
+
+void key_init_default_algorith(void **state)
+{
+	knot_tsig_key_t *key = *state;
+	int r;
+	r = knot_tsig_key_init(key, NULL, "key.name", "Wg==");
+
+	assert_int_equal(r, KNOT_EOK);
+	assert_int_equal(key->algorithm, DNSSEC_TSIG_HMAC_MD5);
+	assert_string_equal(key->name, (uint8_t *)"\x3""key""\x4""name");
+	assert_memory_equal(key->secret.data, (uint8_t *)"\x5a", 1);
+}
+
+void key_init_sha1(void **state)
+{
+	knot_tsig_key_t *key = *state;
+	int r;
+	r = knot_tsig_key_init(key, "hmac-sha1", "knot.dns.", "c2VjcmV0");
+
+	assert_int_equal(r, KNOT_EOK);
+	assert_int_equal(key->algorithm, DNSSEC_TSIG_HMAC_SHA1);
+	assert_string_equal(key->name, (uint8_t *)"\x4""knot""\x3""dns");
+	assert_memory_equal(key->secret.data, (uint8_t *)"secret", 7);
+}
+
+void key_init_str_missing_value(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init_str(&key, NULL);
+	assert_int_equal(r, KNOT_EINVAL);
+}
+
+void key_init_str_malformed(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init_str(&key, "this is malformed");
+	assert_int_not_equal(r, KNOT_EOK);
+}
+
+void key_init_str_invalid_hmac(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init_str(&key, "hmac-sha51299:key:Wg==");
+	assert_int_not_equal(r, KNOT_EOK);
+}
+
+void key_init_str_default_algorithm(void **state)
+{
+	knot_tsig_key_t *key = *state;
+	int r;
+	r= knot_tsig_key_init_str(key, "tsig.key:YmFuYW5ha2V5");
+
+	assert_int_equal(r, KNOT_EOK);
+	assert_int_equal(key->algorithm, DNSSEC_TSIG_HMAC_MD5);
+	assert_string_equal(key->name, (uint8_t *)"\x4""tsig""\x3""key");
+	assert_memory_equal(key->secret.data, (uint8_t *)"bananakey", 9);
+}
+
+void key_init_str_sha384(void **state)
+{
+	knot_tsig_key_t *key = *state;
+	int r;
+	r= knot_tsig_key_init_str(key, "hmac-sha384:strong.key:YXBwbGVrZXk=");
+
+	assert_int_equal(r, KNOT_EOK);
+	assert_int_equal(key->algorithm, DNSSEC_TSIG_HMAC_SHA384);
+	assert_string_equal(key->name, (uint8_t *)"\x6""strong""\x3""key");
+	assert_memory_equal(key->secret.data, (uint8_t *)"applekey", 8);
+}
+
+void key_init_file_no_filename(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init_file(&key, NULL);
+	assert_int_equal(r, KNOT_EINVAL);
+}
+
+
+void key_init_file_not_exists(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r;
+	r = knot_tsig_key_init_file(&key, "/this-really-should-not-exist");
+	assert_int_not_equal(r, KNOT_EOK);
+}
+
+
+
+typedef struct test_context {
+	knot_tsig_key_t *key;
+	char filename[20];
+	int fd;
+} test_context_t;
+
+/*!
+ * \brief setup for test key init from file
+ */
+int setup_file(void **state)
+{
+	*state = malloc(sizeof(test_context_t));
+	test_context_t *ctx = *state;
+	if (ctx == NULL) {
+		return 1;
+	}
+	if (setup((void **)&ctx->key) != 0) {
+		return 2;
+	}
+	strcpy(ctx->filename,"testkey.XXXXXX");
+	ctx->fd = mkstemp(ctx->filename);
+	if(ctx->fd == -1) {
+		fail_msg("failed to create temporary file");
+	}
+	return 0;
+}
+
+int teardown_file(void **state)
+{
+	test_context_t *ctx = *state;
+	close(ctx->fd);
+	unlink(ctx->filename);
+	teardown((void**)&ctx->key);
+	free(ctx);
+	return 0;
+}
+
+void key_init_file_malformed(void **state)
+{
+	test_context_t *ctx = *state;
+	char *content = "malformed";
+
+	write(ctx->fd, content, strlen(content));
+	int r;
+	r= knot_tsig_key_init_file(ctx->key, ctx->filename);
+
+	assert_int_not_equal(r, KNOT_EOK);
+}
+
+void key_init_file_sha512(void **state)
+{
+	test_context_t *ctx = *state;
+	char *content = "hmac-sha512:django.one:V2hvJ3MgdGhhdCBzdHVtYmxpbmcgYX"
+			"JvdW5kIGluIHRoZSBkYXJrPw==\n\n\n";
+
+	write(ctx->fd, content, strlen(content));
+	int r;
+	r= knot_tsig_key_init_file(ctx->key, ctx->filename);
+
+	assert_int_equal(r, KNOT_EOK);
+	assert_int_equal(ctx->key->algorithm, DNSSEC_TSIG_HMAC_SHA512);
+	assert_string_equal(ctx->key->name, (uint8_t *)"\x6""django""\x3""one");
+	assert_memory_equal(ctx->key->secret.data, (uint8_t *) "Who's that "
+			    "stumbling around in the dark?", 40);
+}
+
+void key_init_file_without_newline(void **state)
+{
+	test_context_t *ctx = *state;
+	char *content = "hmac-sha512:django.two:"
+			"UHJlcGFyZSB0byBnZXQgd2luZ2VkIQ==";
+
+	write(ctx->fd, content, strlen(content));
+	int r;
+	r= knot_tsig_key_init_file(ctx->key, ctx->filename);
+
+	assert_int_equal(r, KNOT_EOK);
+	assert_int_equal(ctx->key->algorithm, DNSSEC_TSIG_HMAC_SHA512);
+	assert_string_equal(ctx->key->name, (uint8_t *)"\x6""django""\x3""two");
+	assert_memory_equal(ctx->key->secret.data,
+			    (uint8_t *) "Prepare to get winged!", 22);
+}
+
+void key_init_file_white_spaces(void **state)
+{
+	test_context_t *ctx = *state;
+	char *content = "\thmac-sha1:test:Wg== \n";
+
+	write(ctx->fd, content, strlen(content));
+	int r;
+	r= knot_tsig_key_init_file(ctx->key, ctx->filename);
+
+	assert_int_equal(r, KNOT_EOK);
+	assert_int_equal(ctx->key->algorithm, DNSSEC_TSIG_HMAC_SHA1);
+	assert_string_equal(ctx->key->name, (uint8_t *)"\x4""test");
+	assert_memory_equal(ctx->key->secret.data, (uint8_t *) "\x5a", 1);
+}
+
+void key_copy_invalid(void **state)
+{
+	knot_tsig_key_t key;
+	assert_int_not_equal(knot_tsig_key_copy(NULL, &key), KNOT_EOK);
+	assert_int_not_equal(knot_tsig_key_copy(&key, NULL), KNOT_EOK);
+}
+
+void key_copy_simple(void **state)
+{
+	knot_tsig_key_t key = {
+		.algorithm = DNSSEC_TSIG_HMAC_SHA1,
+		.name = (uint8_t *)"\x4""copy""\x2""me",
+		.secret.size = 6,
+		.secret.data = (uint8_t *)"orange"
+	};
+	knot_tsig_key_t copy = { 0 };
+	assert_int_equal(knot_tsig_key_copy(&copy, &key), KNOT_EOK);
+	assert_int_equal(copy.algorithm, key.algorithm);
+	assert_string_equal(copy.name, key.name);
+	assert_memory_equal(copy.secret.data, key.secret.data, key.secret.size);
+
+	knot_tsig_key_deinit(&copy);
+}
+
+void key_deinit(void **state)
+{
+	knot_tsig_key_t key = { 0 };
+	int r = knot_tsig_key_init(&key, NULL, "a.key.name", "Wg==");
+	assert_int_equal(r, KNOT_EOK);
+
+	uint8_t * data_ptr = key.secret.data;
+
+	knot_tsig_key_deinit(&key);
+
+	knot_tsig_key_t null_key = { 0 };
+	assert_memory_equal(&key, &null_key, sizeof(knot_tsig_key_t));
+
+	assert_int_not_equal(*data_ptr, 0x5a);
+}
+
+
+int main(void)
+{
+	cmocka_set_message_output(CM_OUTPUT_TAP);
+
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(key_init_missing_name),
+		cmocka_unit_test(key_init_missing_secret),
+		cmocka_unit_test(key_init_ivalid_hmac),
+		cmocka_unit_test_setup_teardown(key_init_default_algorith, setup, teardown),
+		cmocka_unit_test_setup_teardown(key_init_sha1, setup, teardown),
+		cmocka_unit_test(key_init_str_missing_value),
+		cmocka_unit_test(key_init_str_malformed),
+		cmocka_unit_test(key_init_str_invalid_hmac),
+		cmocka_unit_test_setup_teardown(key_init_str_default_algorithm, setup, teardown),
+		cmocka_unit_test_setup_teardown(key_init_str_sha384, setup, teardown),
+		cmocka_unit_test(key_init_file_no_filename),
+		cmocka_unit_test(key_init_file_not_exists),
+		cmocka_unit_test_setup_teardown(key_init_file_malformed, setup_file, teardown_file),
+		cmocka_unit_test_setup_teardown(key_init_file_sha512, setup_file, teardown_file),
+		cmocka_unit_test_setup_teardown(key_init_file_without_newline, setup_file, teardown_file),
+		cmocka_unit_test_setup_teardown(key_init_file_white_spaces, setup_file, teardown_file),
+		cmocka_unit_test(key_copy_invalid),
+		cmocka_unit_test(key_copy_simple),
+		cmocka_unit_test(key_deinit),
+	};
+
+	cmocka_run_group_tests(tests, NULL, NULL);
+
+	return 0;
+}
--- tests/zone_events_wrap.c
+++ tests/zone_events_wrap.c
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2014 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2015 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,42 +19,40 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-
 #include "knot/common/evsched.h"
 #include "knot/worker/pool.h"
 #include "knot/zone/events/events.h"
 #include "knot/zone/zone.h"
 #include <syslog.h>
 
-
 // mock objects
 
 struct task *task_assigned;
 
-void __wrap_worker_pool_assign(worker_pool_t *pool, struct task *task)
+void __wrap_worker_pool_assign(worker_pool_t * pool, struct task *task)
 {
-	task_assigned= task;
+	task_assigned = task;
 }
 
 event_t *ev_scheduled;
 
-int __wrap_evsched_schedule(event_t *ev, uint32_t dt)
+int __wrap_evsched_schedule(event_t * ev, uint32_t dt)
 {
 	check_expected(dt);
-	ev_scheduled=ev;
+	ev_scheduled = ev;
 	return KNOT_EOK;
 }
 
-int __wrap_evsched_cancel(event_t *ev){
+int __wrap_evsched_cancel(event_t * ev)
+{
 	check_expected_ptr(ev);
 	return KNOT_EOK;
 }
 
-
 /*!
  * \brief Wrapper for all event callbacks
  */
-int event_callback_wrapper(zone_event_type_t type, zone_t *zone)
+int event_callback_wrapper(zone_event_type_t type, zone_t * zone)
 {
 	check_expected(type);
 	check_expected_ptr(zone);
@@ -62,18 +60,19 @@ int event_callback_wrapper(zone_event_type_t type, zone_t *zone)
 	return mock();
 }
 
-int __wrap_log_msg_zone(int priority, const knot_dname_t *zone, const char *fmt, ...){
+int __wrap_log_msg_zone(int priority, const knot_dname_t * zone,
+			const char *fmt, ...)
+{
 	check_expected(priority);
 	return 0;
 }
 
-
 //! \brief context for each testcase
-typedef struct group_context {
+typedef struct test_context  {
 	evsched_t sched;
 	worker_pool_t *pool;
 	zone_t zone;
-} group_context_t;
+} test_context _t;
 
 //! \brief create test context
 int setup(void **state)
@@ -81,43 +80,49 @@ int setup(void **state)
 	ev_scheduled = NULL;
 	task_assigned = NULL;
 
-	*state = malloc(sizeof(group_context_t));
-	if (state == NULL)
+	*state = malloc(sizeof(test_context _t));
+	if (state == NULL) {
 		return 1;
+	}
 
-	group_context_t *ctx = *state;
+	test_context _t *ctx = *state;
 
 	evsched_init(&ctx->sched, NULL);
 
 	ctx->pool = worker_pool_create(1);
-	if (ctx->pool == NULL)
+	if (ctx->pool == NULL) {
 		return 2;
-	if (zone_events_init(&ctx->zone) != KNOT_EOK)
+	}
+	if (zone_events_init(&ctx->zone) != KNOT_EOK) {
 		return 3;
-	if (zone_events_setup(&ctx->zone ,ctx->pool, &ctx->sched, NULL) != KNOT_EOK)
+	}
+	if (zone_events_setup(&ctx->zone, ctx->pool, &ctx->sched, NULL) !=
+	    KNOT_EOK) {
 		return 4;
+	}
 	return 0;
 }
 
 //! \brief destroy test context
 int teardown(void **state)
 {
-	group_context_t *ctx = *state;
+	test_context _t *ctx = *state;
 
-	expect_value(__wrap_evsched_cancel,ev,ctx->zone.events.event);
+	expect_value(__wrap_evsched_cancel, ev, ctx->zone.events.event);
 	zone_events_deinit(&ctx->zone);
 
 	worker_pool_destroy(ctx->pool);
 	evsched_deinit(&ctx->sched);
 	free(ctx);
-	*state=NULL;
+	*state = NULL;
 	return 0;
 }
 
-void one_correct_task_scheduled(void **state){
-	group_context_t *ctx = *state;
+void one_correct_task_scheduled(void **state)
+{
+	test_context _t *ctx = *state;
 
-	expect_value(__wrap_evsched_schedule,dt, 10*1000);
+	expect_value(__wrap_evsched_schedule, dt, 10 * 1000);
 	zone_events_schedule(&ctx->zone, ZONE_EVENT_RELOAD, 10);
 
 	ev_scheduled->cb(ev_scheduled);
@@ -130,14 +135,15 @@ void one_correct_task_scheduled(void **state){
 }
 
 //! \brief second task is scheduled before the first
-void two_correct_task_scheduled(void **state){
-	group_context_t *ctx = *state;
+void two_correct_task_scheduled(void **state)
+{
+	test_context _t *ctx = *state;
 
-	expect_value(__wrap_evsched_schedule,dt, 10*1000);
+	expect_value(__wrap_evsched_schedule, dt, 10 * 1000);
 	zone_events_schedule(&ctx->zone, ZONE_EVENT_RELOAD, 10);
 
-	expect_value(__wrap_evsched_schedule,dt, 8*1000);
-	expect_value(__wrap_evsched_schedule,dt, 10*1000);
+	expect_value(__wrap_evsched_schedule, dt, 8 * 1000);
+	expect_value(__wrap_evsched_schedule, dt, 10 * 1000);
 	zone_events_schedule(&ctx->zone, ZONE_EVENT_REFRESH, 8);
 
 	ev_scheduled->cb(ev_scheduled);
@@ -156,10 +162,11 @@ void two_correct_task_scheduled(void **state){
 	task_assigned->run(task_assigned);
 }
 
-void double_call_event_wrap(void **state){
-	group_context_t *ctx = *state;
+void double_call_event_wrap(void **state)
+{
+	test_context _t *ctx = *state;
 
-	expect_value(__wrap_evsched_schedule,dt, 10*1000);
+	expect_value(__wrap_evsched_schedule, dt, 10 * 1000);
 	zone_events_schedule(&ctx->zone, ZONE_EVENT_RELOAD, 10);
 
 	ev_scheduled->cb(ev_scheduled);
@@ -172,7 +179,7 @@ void double_call_event_wrap(void **state){
 	task_assigned->run(task_assigned);
 	task_assigned->run(task_assigned);
 
-	expect_value(__wrap_evsched_schedule,dt, 10000*1000);
+	expect_value(__wrap_evsched_schedule, dt, 10000 * 1000);
 	zone_events_schedule(&ctx->zone, ZONE_EVENT_XFER, 10000);
 
 	ev_scheduled->cb(ev_scheduled);
@@ -185,17 +192,16 @@ void double_call_event_wrap(void **state){
 	task_assigned->run(task_assigned);
 }
 
-
-
 /*!
  * \brief Schedule all events and check the right order
  */
-void all_events(void **state){
-	group_context_t *ctx = *state;
+void all_events(void **state)
+{
+	test_context _t *ctx = *state;
 
-	expect_value_count(__wrap_evsched_schedule,dt, 1000, -1);
+	expect_value_count(__wrap_evsched_schedule, dt, 1000, -1);
 
-	for(int i = ZONE_EVENT_INVALID +1; i< ZONE_EVENT_COUNT; ++i) {
+	for (int i = ZONE_EVENT_INVALID + 1; i < ZONE_EVENT_COUNT; ++i) {
 		zone_events_schedule(&ctx->zone, i, 1);
 		expect_value(event_callback_wrapper, type, i);
 		expect_value(event_callback_wrapper, zone, &ctx->zone);
@@ -203,16 +209,17 @@ void all_events(void **state){
 
 	will_return_always(event_callback_wrapper, KNOT_EOK);
 
-	for(int i = 0; i < ZONE_EVENT_COUNT; ++i){
+	for (int i = 0; i < ZONE_EVENT_COUNT; ++i) {
 		ev_scheduled->cb(ev_scheduled);
 		task_assigned->run(task_assigned);
 	}
 }
 
-void freeze(void **state){
-	group_context_t *ctx = *state;
+void freeze(void **state)
+{
+	test_context _t *ctx = *state;
 
-	expect_value(__wrap_evsched_cancel,ev,ctx->zone.events.event);
+	expect_value(__wrap_evsched_cancel, ev, ctx->zone.events.event);
 
 	zone_events_freeze(&ctx->zone);
 
@@ -223,16 +230,17 @@ void freeze(void **state){
 	zone_events_enqueue(&ctx->zone, ZONE_EVENT_REFRESH);
 }
 
-void zone_events_enqueue_running(void **state){
-	group_context_t *ctx = *state;
+void zone_events_enqueue_running(void **state)
+{
+	test_context _t *ctx = *state;
 	time_t now = time(NULL);
 
-	expect_value(__wrap_evsched_schedule,dt, 0);
+	expect_value(__wrap_evsched_schedule, dt, 0);
 	zone_events_schedule_at(&ctx->zone, ZONE_EVENT_EXPIRE, now - 100);
 
 	ev_scheduled->cb(ev_scheduled);
 
-	expect_value(__wrap_evsched_schedule,dt, 0);
+	expect_value(__wrap_evsched_schedule, dt, 0);
 	zone_events_enqueue(&ctx->zone, ZONE_EVENT_RELOAD);
 
 	expect_value(event_callback_wrapper, type, ZONE_EVENT_EXPIRE);
@@ -248,8 +256,9 @@ void zone_events_enqueue_running(void **state){
 	task_assigned->run(task_assigned);
 }
 
-void zone_events_enqueue_not_running(void **state){
-	group_context_t *ctx = *state;
+void zone_events_enqueue_not_running(void **state)
+{
+	test_context _t *ctx = *state;
 
 	zone_events_enqueue(&ctx->zone, ZONE_EVENT_EXPIRE);
 
@@ -259,7 +268,7 @@ void zone_events_enqueue_not_running(void **state){
 	expect_value(event_callback_wrapper, zone, &ctx->zone);
 	will_return(event_callback_wrapper, KNOT_EOK);
 
-	expect_value(__wrap_evsched_schedule,dt, 0);
+	expect_value(__wrap_evsched_schedule, dt, 0);
 
 	assert_non_null(task_assigned);
 	task_assigned->run(task_assigned);
@@ -281,13 +290,13 @@ int main(void)
 	cmocka_set_message_output(CM_OUTPUT_TAP);
 
 	const struct CMUnitTest tests[] = {
-		cmocka_unit_test_setup_teardown(one_correct_task_scheduled,setup,teardown),
-		cmocka_unit_test_setup_teardown(two_correct_task_scheduled,setup,teardown),
-		cmocka_unit_test_setup_teardown(double_call_event_wrap,setup,teardown),
-		cmocka_unit_test_setup_teardown(all_events,setup,teardown),
-		cmocka_unit_test_setup_teardown(zone_events_enqueue_running,setup,teardown),
-		cmocka_unit_test_setup_teardown(zone_events_enqueue_not_running,setup,teardown),
-		cmocka_unit_test_setup_teardown(freeze,setup,teardown),
+		cmocka_unit_test_setup_teardown(one_correct_task_scheduled, setup, teardown),
+		cmocka_unit_test_setup_teardown(two_correct_task_scheduled, setup, teardown),
+		cmocka_unit_test_setup_teardown(double_call_event_wrap, setup, teardown),
+		cmocka_unit_test_setup_teardown(all_events, setup, teardown),
+		cmocka_unit_test_setup_teardown(zone_events_enqueue_running, setup, teardown),
+		cmocka_unit_test_setup_teardown(zone_events_enqueue_not_running, setup, teardown),
+		cmocka_unit_test_setup_teardown(freeze, setup, teardown),
 	};
 
 	cmocka_run_group_tests(tests, NULL, NULL);
@@ -295,43 +304,42 @@ int main(void)
 	return 0;
 }
 
-
-int __wrap_event_reload(zone_t *zone)
+int __wrap_event_reload(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_RELOAD,zone);
+	return event_callback_wrapper(ZONE_EVENT_RELOAD, zone);
 }
 
-int __wrap_event_refresh(zone_t *zone)
+int __wrap_event_refresh(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_REFRESH,zone);
+	return event_callback_wrapper(ZONE_EVENT_REFRESH, zone);
 }
 
-int __wrap_event_xfer(zone_t *zone)
+int __wrap_event_xfer(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_XFER,zone);
+	return event_callback_wrapper(ZONE_EVENT_XFER, zone);
 }
 
-int __wrap_event_update(zone_t *zone)
+int __wrap_event_update(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_UPDATE,zone);
+	return event_callback_wrapper(ZONE_EVENT_UPDATE, zone);
 }
 
-int __wrap_event_expire(zone_t *zone)
+int __wrap_event_expire(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_EXPIRE,zone);
+	return event_callback_wrapper(ZONE_EVENT_EXPIRE, zone);
 }
 
-int __wrap_event_flush(zone_t *zone)
+int __wrap_event_flush(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_FLUSH,zone);
+	return event_callback_wrapper(ZONE_EVENT_FLUSH, zone);
 }
 
-int __wrap_event_notify(zone_t *zone)
+int __wrap_event_notify(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_NOTIFY,zone);
+	return event_callback_wrapper(ZONE_EVENT_NOTIFY, zone);
 }
 
-int __wrap_event_dnssec(zone_t *zone)
+int __wrap_event_dnssec(zone_t * zone)
 {
-	return event_callback_wrapper(ZONE_EVENT_DNSSEC,zone);
+	return event_callback_wrapper(ZONE_EVENT_DNSSEC, zone);
 }
