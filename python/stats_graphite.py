e45c30c7ed6e693c41dcdd1c91a87aeea095f875
Filip Široký <filip.siroky@nic.cz>
2016-11-30 16:39:06 +0100

    correct statistics exporting to influxdb in progress

--- python/stats_graphite.py
+++ python/stats_graphite.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 
-"""Simple program for exposing statistics from Knot DNS over HTTP/HTTPS."""
+"""Simple program for exporting statistics from Knot DNS to influxdb."""
 
 import libknot.control
 import graphyte
 import time
+import json
+import io
+import os
 
 # Configuration.
-#libknot.control.load_lib("../src/.libs/libknot.so")
+libknot.control.load_lib("../src/.libs/libknot.so")
 ctl_socket = "/tmp/knot.sock"
 ctl_timeout = 2
-ctl_flags = "" # set "F" for all supported counters.
-host = "127.0.0.1"
-port = 8080
+ctl_flags = "F" # set "F" for all supported counters.
+host = "217.31.192.164"
+port = "8086"
 # send metrics every x seconds
 interval = 10
-#metrics naming option
-prefix = "Knot"
+#name of database
+DB = "KnotDNS"
+#instance - in case of more servers
+instance = "Knot1"
 
 
 def send():
@@ -44,24 +49,48 @@ def send():
     # Disconnect from the server.
     ctl.send(libknot.control.KnotCtlType.END)
     ctl.close()
+    output = io.StringIO()
 
     # Publish the stats.
     stats = {**global_stats, **zone_stats}
-    for source in stats:
+    #try:
+    timestamp = str(int(time.time()))
+    data = ""
+    for metric in stats["server"]:
+       if data is not "":
+          data = data + ","
+       data = data + metric + "=" + stats["server"][metric]
+    print("server,instance=" + instance + " " + data + " " + timestamp, file=output)
+    '''for source in stats:
         for metric in stats[source]:
-               graphyte.send(source + "." + metric, int(stats[source][metric]))
+            if type(stats[source][metric]) is not dict:
+               print(source +  ",server=" + instance + " " + mertic + "=" )
+               #graphyte.send(source + "." + metric, int(stats[source][metric]))
+            else:
+                for context in stats[source][metric]:
+                    if type(stats[source][metric][context]) is not dict: 
+                        #graphyte.send(source + "." + metric + "." + context, int(stats[source][metric][context]))
+                    else:
+                        for group in stats[source][metric][context]:
+                            if type(stats[source][metric][context][group]) is not dict: 
+                                #graphyte.send(source + "." + metric + "." + context + "." + group, int(stats[source][metric][context][group]))
+                            else:
+                                for mtype in stats[source][metric][context][group]:
+                                    if type(stats[source][metric][context][group][mtype]) is not dict: 
+                                        #graphyte.send(source + "." + metric + "." + context + "." + group + "." + mtype, int(stats[source][metric][context][group][mtype]))
+    '''
+    print(output.getvalue())
+    bin_data = output.getvalue()
+    os.system("curl -i -XPOST 'http://"+host+":"+port+"/write?db="+DB+"' --data-binary '" + bin_data + "'")
+    
     return
-
-
-graphyte.init(host=host, port=port, prefix=prefix)
 
 print("%s: Graphite sender - Server Start - %s:%s" %
       (time.asctime(), host, port))
 
 try:
-    while(True):
-        send()
+   while(True):
+      send()
+      time.sleep(interval)
 except KeyboardInterrupt:
-    pass
-
-httpd.server_close()
+   pass
